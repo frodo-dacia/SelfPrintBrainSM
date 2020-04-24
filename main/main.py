f888c91254066b8a8fca60d6fec70ce392a64ad9
Puscalau Robert <robert-george.puscalau@sutdent.ac.tuiasi.ro>
2020-04-24 21:45:56 +0300

    added PrintingService module

--- main/main.py
+++ main/main.py
@@ -1,1 +1,11 @@
 #file that contains main function for the printing server
+
+def main():
+    #start printing server
+    #start gui
+    #on separate threads
+    #the 2 modules have to communicate
+    print("main")
+
+if __name__ == "__main__":
+    main()
